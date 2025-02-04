480e325beea0967805ea8a93c2307972394922e0
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2025-02-04 22:17:46 +0300

    Add files via upload

---
+++ Alert.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: ()=>void;
+}
+export const Alert = ({ children,onClose }: Props) => {
+  return (
+    <div
+      className="alert alert-primary alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
